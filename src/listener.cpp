2a84ce9fd6378b35e2e14c062e8b786116cbc0e6
Keitaro Nakamura <keitaro@hogehoge.com>
2023-12-30 15:55:39 +0900

    update code

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/Vector3.hpp"
+#include "joint_msgs/msg/joint_angle.hpp"
 using std::placeholders::_1;
 
 class Listener : public rclcpp::Node
@@ -10,16 +10,16 @@ class Listener : public rclcpp::Node
     Listener()
     : Node("listener")
     {
-      subscription_ = this->create_subscription<geometry_msgs::msg::Vector3>(
-      "countup", 10, std::bind(&Listener::topic_callback, this, _1));
+      listening_ = this->create_subscription<joint_msgs::msg::JointAngle>(
+      "/joint_angle", 10, std::bind(&Listener::topic_callback, this, _1));
     }
 
   private:
-    void topic_callback(const geometry_msgs::msg::Vector3 & msg) const
+    void topic_callback(const joint_msgs::msg::JointAngle & message) const
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%lf', '%lf', '%lf'", msg.x, msg.y, msg.z);
+      RCLCPP_INFO(this->get_logger(),"Subscripted::[1]: '%lf', [2]: '%lf', [3]: '%lf', [4]: '%lf', [5]: '%lf', [6]: '%lf', [7]: '%lf'", message.joint1, message.joint2, message.joint3, message.joint4, message.joint5, message.joint6, message.joint7);
     }
-    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr subscription_;
+    rclcpp::Subscription<joint_msgs::msg::JointAngle>::SharedPtr listening_;
 };
 
 int main(int argc, char * argv[])
