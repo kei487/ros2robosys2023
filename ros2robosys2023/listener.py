27f00a8fb048070a89e3c4a0e42eb8fe1c33779a
Keitaro Nakamura <keitaro.nakamura@hoge.com>
2024-01-04 18:53:07 +0900

    init

---
+++ ros2robosys2023/listener.py
@@ -0,0 +1,12 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+def callback(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
+
+rclpy.init()
+node = Node("listener")
+sub = node.create_subscription(Int16, "countup", callback, 10)
+rclpy.spin(node)
