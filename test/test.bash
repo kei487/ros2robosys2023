e26894749e7a973ece9d60f7f293c0006817a67e
Keitaro Nakamura <keitaro.nakamura@hoge.com>
2024-01-04 22:25:13 +0900

    update test

--- test/test.bash
+++ test/test.bash
@@ -7,8 +7,9 @@ dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
 cd $dir/ros2_ws
+colcon build
 source install/setup.bash
-timeout 10 ros2 launch ros2robosys2023 test.launch.py > /tmp/ros2robosys2023.log
+timeout 10 ros2 launch ros2robosys2023 example.launch.xml > /tmp/ros2robosys2023.log
 
 cat /tmp/ros2robosys2023.log |
 grep 'Listen: 10' 
